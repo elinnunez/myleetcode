7e595b852b99e915243b1b2fc533e4d324655229
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-05-23 19:17:21 -0500

    Time: 46 ms (64.77%), Space: 20 MB (34.24%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -13,8 +13,7 @@ public:
             int clow = intervals[i][0];
             int chi = intervals[i][1];
             
-            
-            if(!(clow <= curmax)) {
+            if(clow > curmax) {
                 vector<int> temp;
                 temp.push_back(low);
                 temp.push_back(curmax);
