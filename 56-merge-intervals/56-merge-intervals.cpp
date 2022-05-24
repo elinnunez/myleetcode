2962068c751f39f292cc3a1f28672ced32f77e3e
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-05-23 19:15:19 -0500

    Time: 60 ms (33.41%), Space: 20 MB (27.38%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end());
+        
+        vector<vector<int>> bag;
+        
+        int low = intervals[0][0];
+        int curmax = intervals[0][1];
+        
+        for(int i = 1; i < intervals.size(); i++) {
+            int clow = intervals[i][0];
+            int chi = intervals[i][1];
+            
+            
+            if(!(clow <= curmax)) {
+                vector<int> temp;
+                temp.push_back(low);
+                temp.push_back(curmax);
+                bag.push_back(temp);
+                low = clow;
+            }
+            curmax = max(curmax,chi);
+        }
+        
+        vector<int> temp;
+        temp.push_back(low);
+        temp.push_back(curmax);
+        
+        bag.push_back(temp);
+        
+        return bag;
+    }
+};
