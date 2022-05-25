900232afdf0e164bb6283040a5a2e4eda7cfac8f
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-05-24 19:03:34 -0500

    Time: 491 ms (23.28%), Space: 82.2 MB (15.81%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        
+        vector<vector<int>> temp;
+        
+        for(int i = 0; i < points.size(); i++) {
+            int euc = pow(points[i][0],2) + pow(points[i][1],2);
+        
+            vector<int> bag = {euc, i};
+            temp.push_back(bag);
+        }
+        
+        sort(temp.begin(), temp.end());
+        
+        vector<vector<int>> kclose;
+        
+        int count = 0;
+        
+        while(count != k) {
+            kclose.push_back(points[temp[count][1]]);
+            count++;
+            
+        }
+        
+        return kclose;
+    }
+};
