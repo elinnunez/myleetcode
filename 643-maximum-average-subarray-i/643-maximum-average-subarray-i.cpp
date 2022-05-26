dfdf0cedcfb179eec5cb798df47131d9730dd4fd
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-05-26 18:25:08 -0500

    Time: 248 ms (40.99%), Space: 109.6 MB (62.04%) - LeetHub

--- 643-maximum-average-subarray-i/643-maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
         
-        double maxAvg = INT_MIN;
+        double maxAvg = INT_MIN; // set max sum average to the lowest int possible
         
-        int windowStart = 0;
-        double windowSum = 0;
+        int windowStart = 0; // to keep track of which index to delete from sum
+        double windowSum = 0; // to sum up the numbers
         
         for(int windowEnd = 0; windowEnd < nums.size(); windowEnd++) {
             
             windowSum+= nums[windowEnd];
             
-            if(windowEnd >= k-1) {
-                double curAvg = windowSum/k;
-                maxAvg = max(curAvg, maxAvg);
-                windowSum -= nums[windowStart];
-                windowStart++;
+            if(windowEnd >= k-1) { // if reached k amount start doing work
+                double curAvg = windowSum/k; // calculate average sum
+                maxAvg = max(curAvg, maxAvg); // compare to current average vs max average to get the max from both
+                windowSum -= nums[windowStart]; // subtract from left side of window
+                windowStart++; // increment counter by 1 for next time when index needs to be subtracted
             }
         }
         
@@ -23,3 +23,5 @@ public:
         
     }
 };
+
+// Sliding Window
