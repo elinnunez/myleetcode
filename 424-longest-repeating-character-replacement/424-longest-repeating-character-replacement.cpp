34b054ade64fa15d12e81d958f3ebfb706ef9932
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-05-27 20:53:16 -0500

    Time: 23 ms (36.80%), Space: 7.1 MB (15.13%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -30,3 +30,14 @@ public:
         
     }
 };
+
+
+// s = "ABAB" k = 2: left = 0; maxLength = 0; maxRepeat = 0;
+// i = 0; curchar = A; hash[A] = 1; maxRepeat = max(0,1) =  1; maxLength = max(0, 0 - 0 + 1) = 1; left = 0;
+//    if((0 - 0 + 1 - 1) -> 0 > 2)...
+// i =  1; curChar = B; hash[B] = 1; maxRepeat = max(1,1) = 1; maxLength = max(1, 1- 0 + 1) = 2; left = 0;
+//    if(1 - 0 + 1 - 1 -> 1 > 2)...
+// i = 2; curChar = A; hash[A] = 2, maxRepeat = max(1,2) = 2; maxLength = max(2, 2 - 0 + 1) = 3; left = 0;
+//    if(2 - 0 + 1 - 2 -> 1 > 2)
+// i = 3; curChar = B; hash[B] = 2; maxRepeat = max(2, 2) = 2; maxLength = max(3, 3 - 0 + 1) = 4; left = 0;
+//    if(3 - 1 + 1 - 2 -> 1 > 2)...
