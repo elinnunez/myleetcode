e02cf93428f474d8fc6a75a90b95f1a01b3ddab6
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-05-29 12:02:29 -0500

    Time: 16 ms (45.60%), Space: 7.3 MB (47.01%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         
-        int left = 0;
-        
         unordered_map<char,int> hash;
         
         for(int i = 0; i < s1.size(); i++) {
@@ -11,6 +9,8 @@ public:
         }
         
         int matches = 0;
+        
+        int left = 0;
         
         for(int r = 0; r < s2.size(); r++) {
             char curChar = s2[r];
@@ -44,34 +44,3 @@ public:
         
     }
 };
-
-
-// class Solution {
-// public:
-//     bool checkInclusion(string s1, string s2) {
-        
-//         int left = 0;
-        
-//         sort(s1.begin(), s1.end()); // O(nlogn)
-        
-//         for(int r = 0; r < s2.size(); r++) { // O(n)
-            
-//             if(r - left == s1.size() - 1) {
-//                 string temp = s2.substr(left, r-left+1);
-//                 sort(temp.begin(), temp.end()); // O(nlogn)
-                
-//                 if(temp == s1) {
-//                     return true;
-//                 }
-                
-//                 left++;
-//             }
-//         }
-        
-//         return false;
-        
-//     }
-// };
-
-// // Brute Force Solution
-// // Time Complexity: O(nlogn + n*(nlogn)) = O(n*nlogn)
