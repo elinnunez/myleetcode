5c8ae0657b2ace766857d16f008527633237d69d
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-05-30 16:50:29 -0500

    Time: 28 ms (49.61%), Space: 16.5 MB (15.76%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -14,7 +14,7 @@ public:
         
         int matches = 0;
         
-        int curWin = INT_MAX;
+        int curWin = INT_MAX; //  Set window to larget possible size
         
         for(const auto& it: t) {
             hash[it]++;
@@ -23,7 +23,7 @@ public:
         for(int r = 0; r < s.size(); r++) {
             char curChar = s[r];
             
-            if(string(1, curChar) == t) {
+            if(string(1, curChar) == t) { // if t is a single character and is found, that's the lowest that can be returned
                 return t;
             }
             
@@ -36,23 +36,23 @@ public:
             
             while(matches == t.size()) {
                 
-                int winSize = r-left+1;
+                int winSize = r-left+1; // current window size that has all matches
                 
-                if(winSize < curWin || out == "") {
-                    out = s.substr(left, winSize);
-                    curWin = winSize;
+                if(winSize < curWin || out == "") { // check if the current winSize is smaller than previous curWin
+                    out = s.substr(left, winSize); // output is now a smaller substring that still has all matches
+                    curWin = winSize; // set curWin to newer smaller window size
                 }
                 
                 char leftChar = s[left];
                 
                 if(hash.count(leftChar)) {
-                    if(hash[leftChar] == 0) {
+                    if(hash[leftChar] == 0) { // if the hash was emptied because of matches, decrement matches by 1 before putting left char back
                         matches--;
                     }
-                    hash[leftChar]++;
+                    hash[leftChar]++; // return the count back to hash before incrementing left
                 }
                     
-                left++;
+                left++; // increment left by 1
             }
         }
         
