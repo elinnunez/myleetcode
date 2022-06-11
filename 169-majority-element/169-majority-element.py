dbcb1ca76939a7d832d378e8996efbfd096dd7b1
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-06-11 14:06:26 -0500

    Time: 300 ms (17.42%), Space: 15.6 MB (33.81%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,16 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-      map = {}
-    
-      majority = len(nums) // 2
+        map = {}
         
-      for element in nums:
-        if element not in map:
-          map[element] = 1
-        else:
-          map[element] += 1
-        # If an element appears more than n/2 times, return that element
-        if map[element] > majority:
-          return element
-
-      return None
+        majority = len(nums) // 2
+        
+        for element in nums:
+            if element not in map:
+                map[element] = 1
+            else:
+                map[element] += 1
+            # If an element appears more than n/2 times, return that element
+            if map[element] > majority:
+                return element
+        
+        return None
