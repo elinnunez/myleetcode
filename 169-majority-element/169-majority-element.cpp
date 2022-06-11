12df5eb6c4513049376d4ae8b79c8af14d536920
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-06-11 14:00:36 -0500

    Time: 22 ms (76.07%), Space: 19.6 MB (13.93%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -3,10 +3,12 @@ public:
     int majorityElement(vector<int>& nums) {
         unordered_map<char,int> hash;
         
+        int majority = nums.size()/2;
+        
         for(int i = 0; i < nums.size(); i++) {
             hash[nums[i]]++;
             
-            if(hash[nums[i]] > nums.size()/2) {
+            if(hash[nums[i]] > majority) {
                 return nums[i];
             }
         }
