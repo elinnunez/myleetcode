3373cb79391f545cbfde0a1ffdfb7710a977b929
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-06-13 18:49:06 -0500

    Time: 184 ms (60.53%), Space: 51.2 MB (12.39%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -4,23 +4,23 @@ public:
         map<int,int> ht;
         
         for(int i = 0; i < nums.size(); i++) {
-            ht[nums[i]] = 1;
+            ht[nums[i]] = NULL;
         }
         
-        int curMax = INT_MIN;
         int prev = 0;
+        int maxCount = INT_MIN;
         int count = 0;
         
         for(auto const& it: ht) {
             if(it.first - prev > 1) {
-                curMax = max(curMax,count);
+                maxCount = max(maxCount,count);
                 count = 0;
             }
             prev = it.first;
             count+=1;
         }
         
-        return max(curMax,count);
-
+         return max(maxCount,count);
+        
     }
 };
