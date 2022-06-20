a2461eb6dc9e60713c8f66fca86e9d6bb238bfdd
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-06-19 23:06:42 -0500

    Time: 97 ms (35.41%), Space: 42.4 MB (48.69%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -7,13 +7,13 @@ public:
         
         int total = 0;
         
-        for(int i = right; i < n; i++) { // O(k)
+        for(int i = right; i < n; i++) { // O(n)
             total+= cardPoints[i];
         }
         
         int result = total;
         
-        while(right < n) { // O(k)
+        while(right < n) { // O(n)
             total+= (cardPoints[left] - cardPoints[right]);
             result = max(result, total); // O(1)
             left++;
@@ -24,4 +24,4 @@ public:
     }
 };
 
-// Time Complexity: O(k)
+// Time Complexity: O(n) when all cards are counted
