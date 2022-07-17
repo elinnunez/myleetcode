323c42baae4c80c826edd9d54c35e1e4c61bc3b9
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-07-17 10:00:40 -0500

    Time: 111 ms (7.87%), Space: 10.8 MB (15.61%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -3,9 +3,7 @@ public:
     int firstUniqChar(string s) {
         unordered_map<char,bool> unique;
         
-        int firstUnique = -1;
-        
-        for(int i = s.size()-1; i >= 0; i--) {
+        for(int i = 0; i < s.size(); i++) {
             if(!unique.count(s[i])) {
                 unique[s[i]] = true;
             } else {
@@ -13,10 +11,11 @@ public:
             }
         }
         
+        int firstUnique = -1;
+        
         for(int i = 0; i < s.size(); i++) {
             if(unique[s[i]] == true) {
-                firstUnique = i;
-                break;
+                return i;
             }
         }
         
