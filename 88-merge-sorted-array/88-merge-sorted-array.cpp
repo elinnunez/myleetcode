b334e053b828e3b899aa012a4e6397bc2ae86fcd
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-07-17 14:07:30 -0500

    Time: 3 ms (75.18%), Space: 9.1 MB (29.13%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        int vecSize = nums1.size() - 1;
+        int curIdx = nums1.size() - 1;
         
         while(m > 0 && n > 0) {
             if(nums1[m-1] >= nums2[n-1]) {
-                nums1[vecSize] = nums1[m-1];
+                nums1[curIdx] = nums1[m-1];
                 m--;
             } else {
-                nums1[vecSize] = nums2[n-1];
+                nums1[curIdx] = nums2[n-1];
                 n--;
             }
-            vecSize--;
+            curIdx--;
         }
         
         while(n > 0) {
-            nums1[vecSize] = nums2[n-1];
+            nums1[curIdx] = nums2[n-1];
             n--;
-            vecSize--;
+            curIdx--;
         }
     }
 };
