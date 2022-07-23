2b3c027f2e1240da0b98bdf056b2d445275218f2
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-07-23 08:12:48 -0500

    Time: 747 ms (7.82%), Space: 10.3 MB (32.72%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    int countPrimes(int n) {
-        int count = 0;
+    int countPrimes(int n) {     
         if(n <= 1) {
-            return count;
+            return 0;
         }
         
-        vector<bool> sieve(n,true);
+        vector<bool> sieve(n, true);
         sieve[0] = false;
         sieve[1] = false;
         
         // Sieve of Eratosthenes
         for(int i = 2; i*i < n; i++) { // O(n)
-            if(sieve[i] == true) {
+            if (sieve[i]) {
                 for(int j = i*i; j < n; j+=i) { // Harmonic Series: O(logn)
                     sieve[j] = false;
                 }
             }
         }
         
+        int count = 0;
+        
         // Count all numbers that are prime
         for(int i = 2; i < sieve.size(); i++) { // O(n)
-            if(sieve[i] == true) {
+            if(sieve[i]) {
                 count++;
             }
         }
         
         return count;
+        
     }
 };
 
