55465c8c897cf8e868d94b5a8e7aa84ef2b8bae7
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-07-26 11:00:18 -0500

    Time: 4 ms (31.79%), Space: 6.4 MB (10.10%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> s1;
-
-        for(int i = 0; i < s.size(); i++) {
-            if(s1.empty()) {
-                s1.push(s[i]);
-            } else {
-                if(s1.top() == '(' && s[i] == ')' || s1.top() == '{' && s[i] == '}' || s1.top() == '[' && s[i] == ']') {
-                s1.pop();
-            } else {
-                s1.push(s[i]);
-            }
-            }
+        
+        if(s.size() < 2) {
+            return false;
         }
-
-        if(s1.empty()) {
+        
+        stack<char> temp;
+        
+        for(int i = 0; i < s.size(); i++) {
+            char cur = s[i];
+            
+            if(cur == '{' || cur == '[' || cur == '(') {
+                temp.push(cur);
+            } else {
+                if(!temp.empty()) {
+                    if(temp.top() == '(' && cur == ')' || temp.top() == '[' && cur == ']' || temp.top() == '{' && cur == '}') {
+                        temp.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+            
+        }
+        
+        if(temp.empty()) {
             return true;
         }
+        
         return false;
     }
 };
