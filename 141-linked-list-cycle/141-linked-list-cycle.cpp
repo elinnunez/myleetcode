5cf28ddd5e3fb4916ea1f1fe7f0b3b36a19db54e
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-07-26 10:54:01 -0500

    Time: 19 ms (38.14%), Space: 8.1 MB (57.28%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,24 +9,35 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == nullptr || head->next == nullptr) {
+        
+        //if head is empty or the next node is nullptr, not a cycle
+        if (head == nullptr || head->next == nullptr)
+        {
             return false;
         }
-        ListNode* turtle = head;
-        ListNode* hare = head;
         
-        while(hare->next->next != nullptr) {
-            turtle = turtle->next;
-            hare = hare->next->next;
-                
-            if(hare == turtle) {
-                return true;
-            }
-            if(hare->next == nullptr) {
+        ListNode *walker = head;
+        ListNode *runner = head;
+        
+        while (runner != nullptr)
+        {
+            //if runner points at nullptr, not a linked list cycle
+            if (runner->next == nullptr)
+            {
                 return false;
             }
+
+            //runner goes twice as far twice as fast
+            runner = runner->next->next;
+            walker = walker->next;
+
+            //if at some point, runner outlaps walker and they are equal, this means that the list loops
+            if (runner == walker)
+            {
+                return true;
+            }
         }
-        
+        //only returns false if runner gets to a nullptr meaning not a ll cycle
         return false;
     }
 };
