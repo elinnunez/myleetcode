70c6246f7ed17d440e194e2590429ced17953d2f
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-08-02 17:42:23 -0500

    Time: 365 ms (58.93%), Space: 85.3 MB (13.88%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -2,27 +2,40 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         
-        vector<vector<int>> temp;
+        vector<pair<int,vector<int>>> bag;
         
         for(int i = 0; i < points.size(); i++) {
-            int euc = pow(points[i][0],2) + pow(points[i][1],2);
-        
-            vector<int> bag = {euc, i};
-            temp.push_back(bag);
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            int distance = pow(x1-0,2) + pow(y1,2);
+            
+            pair<int,vector<int>> p;
+            p.first = distance;
+            p.second = points[i];
+            
+            bag.push_back(p);
         }
         
-        sort(temp.begin(), temp.end());
+        sort(bag.begin(), bag.end());
         
-        vector<vector<int>> kclose;
+        vector<vector<int>> output;
         
-        int count = 0;
-        
-        while(count != k) {
-            int ptsIdx = temp[count][1];
-            kclose.push_back(points[ptsIdx]);
-            count++;
+        for(int i = 0; i < k; i++) {
+            output.push_back(bag[i].second);
         }
         
-        return kclose;
+        return output;
+        
     }
 };
+
+//points are x1,y1
+// origin are x2,y2
+
+/*
+
+loop through points array
+-> use the euclidean distance formula to calculate the distance
+-> pair{distance,arr[int]}
+->pull the second element and push to separate arary and return k elements;
+*/
