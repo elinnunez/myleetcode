5cd7b06bec70200d473814ce81e4fcf4af8abfe1
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-08-02 14:42:39 -0500

    Time: 19 ms (56.25%), Space: 21.4 MB (98.63%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -15,16 +15,27 @@ public:
         if(!root) {
             return true;
         }
+        // if left node has a val >= root node
         if(left != nullptr && root->val <= left->val) {
             return false;
         }
+        // if right node has a val <= root node
         if(right != nullptr && root->val >= right->val) {
             return false;
         }
         
         return isBST(root->left, left, root) && isBST(root->right, root, right);
     }
+    
     bool isValidBST(TreeNode* root) {
         return isBST(root);
     }
 };
+
+// root = 5, left = null, right = null
+// isbst(1,null,5) = true && isbst(4,5,null) == false ------> true && false == (false)
+//  -> root = 1, left = null, right = 5
+//    -> isbst(null,null,1) && isbst(null,1,5) == true
+//       -> return true      && return true 
+//  -> root = 4, left=5, right = null
+//    -> left != null && 4 <= 5 -> return false
