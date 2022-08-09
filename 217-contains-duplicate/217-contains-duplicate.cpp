f2612cd382242149241eb37d1ba737d6db06c906
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-08-08 21:08:04 -0500

    Time: 199 ms (15.32%), Space: 52.2 MB (35.18%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // Brute Force Solution
-//         sort(nums.begin(), nums.end());
+        set<int> values;
         
-//         for(int i = 0; i < nums.size()-1; i++) {
-//             if(nums[i] == nums[i+1]) {
-//                 return true;
-//             }
-//         }
-        
-//         return false;
-        
-        map<int, int> bag;
-        
-        for(int i = 0; i < nums.size(); i++) {
-            bag[nums[i]]++;
-            if(bag[nums[i]] > 1) {
+        for(int& num : nums) {
+            if(values.count(num)) {
                 return true;
+            } else {
+                values.insert(num);
             }
         }
         
