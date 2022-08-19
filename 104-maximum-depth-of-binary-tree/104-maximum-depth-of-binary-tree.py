05b81a9eed9985014381d7cedfb64a2f59deb436
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-08-18 20:00:51 -0500

    Time: 73 ms (34.04%), Space: 16.4 MB (23.84%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -1,27 +1,14 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int maxDepth(TreeNode* root) {
-        if(root == nullptr) {
-            return 0;
-        } else if (!root->left && !root->right) {
-            return 1;
-        } else if (!root->left) {
-            return maxDepth(root->right) + 1;
-        } else if (!root->right) {
-            return maxDepth(root->left) + 1;
-        } else {
-            return max(maxDepth(root->left)+1, maxDepth(root->right)+1);
-        }
-    }
-};
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        elif not root.left and not root.right:
+            return 1
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        
