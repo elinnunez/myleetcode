eb196bf42999dd802ac9bb20c6dc9424342dc5d2
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-08-19 12:55:51 -0500

    Time: 37 ms (91.50%), Space: 7.7 MB (86.26%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        
+        if(p == "*") {
+            return true;
+        }
+        
+        int m = s.size();
+        int n = p.size();
+        
+        bool dp[m+1][n+1];
+        memset(dp,false,sizeof(dp));
+        
+        dp[0][0] = true;
+        
+        for(int i = 1; i < n+1; i++) {
+            if(p[i-1] != '*') {
+                break;
+            }
+            dp[0][i]=true;
+        }
+        
+        for(int i = 1; i < m+1; i++) {
+            for(int j = 1; j < n+1; j++) {
+                if(s[i-1] == p[j-1] || p[j-1] == '?') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (p[j-1] == '*') {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        } 
+        
+        return dp[m][n];
+    }
+};
+
+// Time Complexity: O(m*n)
+// Space Complexity: O(m*n)
