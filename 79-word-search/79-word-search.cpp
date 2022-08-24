8ded950ac96501767b860608f17695fa468c4950
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-08-24 07:45:44 -0500

    Time: 376 ms (76.58%), Space: 8.1 MB (14.92%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,52 +1,47 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, int i, int j, string word, int idx, vector<vector<bool>>& visited) {
+    bool dfs(vector<vector<char>>& board, int r, int c, int idx, string& word, vector<vector<bool>>& visited) {
         if(idx == word.size()) {
             return true;
         }
         
-        if((i < 0 || i >= board.size()) || j < 0 || j >= board[0].size()) {
-            return false;
-        } 
-        
-        if(visited[i][j] == true || board[i][j] != word[idx]) {
+        if(r < 0 || r >= board.size() || c < 0 || c >= board[r].size()) {
             return false;
         }
         
-        visited[i][j] = true;
+        if(board[r][c] != word[idx] || visited[r][c] == true) {
+            return false;
+        }
         
-        bool isFound = dfs(board,i+1,j,word,idx+1,visited) ||
-        dfs(board,i-1,j,word,idx+1,visited) ||
-        dfs(board,i,j+1,word,idx+1,visited) ||
-        dfs(board,i,j-1,word,idx+1,visited);
+        visited[r][c] = true;
         
-        visited[i][j] = false;
+        bool response = dfs(board,r+1,c,idx+1,word,visited) || 
+                        dfs(board,r-1,c,idx+1,word,visited) || 
+                        dfs(board,r,c+1,idx+1,word,visited) || 
+                        dfs(board,r,c-1,idx+1,word,visited);  
         
-        return isFound;
+        visited[r][c] = false;
+        
+        return response;
     }
     
     bool exist(vector<vector<char>>& board, string word) {
-        
         int m = board.size();
         int n = board[0].size();
         
-        // create the bool vector of m size and of default value (type vector<bool>, each having size n and default values false)
         vector<vector<bool>> visited(m, vector<bool>(n,false));
-        
-        bool exists = false;
         
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(board[i][j] == word[0]) {
-                    exists = dfs(board,i,j,word,0,visited);
-                    if(exists == true) {
+                    if(dfs(board,i,j,0,word,visited)) {
                         return true;
                     }
                 }
             }
         }
         
-        return exists;
+        return false;
     }
 };
 // Time Complexity: O(mn)
