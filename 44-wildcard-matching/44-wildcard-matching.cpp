f05a8854345288930c39a77fe9e185a65625892e
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-08-24 07:53:50 -0500

    Time: 46 ms (87.24%), Space: 7.7 MB (86.26%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        
         if(p == "*") {
             return true;
         }
@@ -18,24 +17,24 @@ public:
             if(p[i-1] != '*') {
                 break;
             }
-            dp[0][i]=true;
+            dp[0][i] = true;
         }
         
         for(int i = 1; i < m+1; i++) {
             for(int j = 1; j < n+1; j++) {
                 if(s[i-1] == p[j-1] || p[j-1] == '?') {
                     dp[i][j] = dp[i-1][j-1];
-                } else if (p[j-1] == '*') {
+                } else if(p[j-1] == '*') {
                     dp[i][j] = dp[i-1][j] || dp[i][j-1];
                 } else {
                     dp[i][j] = false;
                 }
             }
-        } 
+        }
         
         return dp[m][n];
+        
     }
 };
-
-// Time Complexity: O(m*n)
-// Space Complexity: O(m*n)
+// Time Complexity: O(mn)
+// Space Complexity: O(mn)
