489b5002d243bb30d08abf37f244b10cd43c8901
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-09-01 21:24:25 -0500

    Time: 67 ms (5.67%), Space: 13.5 MB (10.53%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<vector<int>> zeroElt;
-        
-        int count = 0;
+        set<pair<int,int>> zeroElt;
         
         for(int i = 0; i < matrix.size(); i++) {
             for(int j = 0; j < matrix[i].size(); j++) {
                 if(matrix[i][j] == 0) {
-                    zeroElt.insert({i,j});
-                    count++;
+                    zeroElt.insert(make_pair(i,j));
                 }
             }
         }
         
         for(int i = 0; i < matrix.size(); i++) {
             for(int j = 0; j < matrix[i].size(); j++) {
-                if(matrix[i][j] == 0 && zeroElt.count({i,j})) {
-                    for(int k = 0; k < matrix.size(); k++) {
-                        matrix[k][j] = 0;
+                for(const auto& pair : zeroElt) {
+                    if(pair.first == i || pair.second == j) {
+                        matrix[i][j] = 0;
                     }
-                    for(int l = 0; l < matrix[i].size(); l++) {
-                        matrix[i][l] = 0;
-                    }
-                    count--;
-                }
-                if(count == 0) {
-                    return;
                 }
             }
         }
@@ -34,3 +24,5 @@ public:
         return;
     }
 };
+// Time Complexity: O(mno) where o = size of set
+// Space Complexity: O(1)
