361e84937f54b051dbe4aaafc59788928fb66197
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-09-04 15:19:24 -0500

    Time: 313 ms (77.57%), Space: 27.2 MB (43.27%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -1,25 +1,27 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        minSize = len(nums)+1
-        curSum = 0
-        windowStart = 0
+        minSize = len(nums)+1 # initialize a max value
+        winSum = 0 # set current sum to 0
+        windowStart = 0 # set begining of window to index 0
         
         for windowEnd in range(len(nums)):
             
-            if nums[windowEnd] == target:
+            if nums[windowEnd] >= target: # if the value is >= to target return 1 since that's the smallest an array can be
                 return 1
             
-            curSum+= nums[windowEnd]
+            winSum+= nums[windowEnd]
                 
-            while curSum >= target:
-                curSize = windowEnd - windowStart + 1
-                minSize = min(minSize, curSize)
-                curSum-= nums[windowStart]
-                windowStart+=1
+            while winSum >= target: # keep looping while the sum is >= to target
+                winSize = windowEnd - windowStart + 1 # get current size of window
+                minSize = min(minSize, winSize) # get min window size from prev minSize and current size
+                winSum-= nums[windowStart] # subtract the winSum from the left as its being removed from windows
+                windowStart+=1 # increment windowStart from the left
                 
         
-        if minSize == len(nums)+1:
+        if minSize == len(nums)+1: # if nothing was found and minSize is still at default max value return 0
             return 0
         
         return minSize
     
+# Time Complexity: O(n)
+# Space Complexity: O(1)
