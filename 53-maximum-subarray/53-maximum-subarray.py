7a6fe31e3573f7d5215e0fed57dc3a174922687f
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-09-07 20:55:46 -0500

    Time: 712 ms (98.09%), Space: 27.8 MB (96.24%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,28 +1,17 @@
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
         
-        if(nums.size() == 1) {
-            return nums[0];
-        }
+        curMax = -math.inf
+        curSum = 0
         
-        int curMax = INT_MIN;
-        int curSum = 0;
-        
-        int i = 0;
-        
-        while(i < nums.size()) {
-            curSum+= nums[i];
+        for num in nums:
+            curSum+= num
             
-            curMax = max(curSum, curMax);
+            curMax = max(curMax,curSum)
             
-            if(curSum < 0) {
-                curSum = 0;
-            }
-            
-            i++;
-        }
+            if curSum < 0:
+                curSum = 0
         
-        return curMax;
-    }
-};
+        return curMax
