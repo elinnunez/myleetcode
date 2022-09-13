bb874acfc0dda457299fbcdb7c347165b9467929
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-09-12 23:59:19 -0500

    Time: 53 ms (81.84%), Space: 14.6 MB (7.96%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,38 +1,30 @@
-class Solution {
-public:
-    bool binSearch(vector<int>& arr, int& target, int i, int j) {
-        if(i > j) {
-            return false;
-        }
+class Solution: 
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-        int mid = i+(j-i)/2;
+        def binSearch(mat,target,i,j):
+            if i > j:
+                return False
+            
+            mid = i + (j-i)//2;
+            
+            if mat[mid] == target:
+                return True
+            else:
+                if mat[mid] > target:
+                    return binSearch(mat,target,i,mid-1)
+                else:
+                    return binSearch(mat,target,mid+1,j)
         
-        if(arr[mid] == target) {
-            return true;
-        } else {
-            if(arr[mid] > target) {
-                return binSearch(arr,target,i,mid-1);
-            } else {
-                return binSearch(arr,target,mid+1,j);
-            }
-        }
-    }
-    
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        m = len(matrix)
+            
+        for i in range(m):
+            n = len(matrix[i])
+            
+            if matrix[i][0] == target or matrix[i][n-1] == target:
+                return True
+            
+            if matrix[i][0] < target and target < matrix[i][n-1]:
+                return binSearch(matrix[i],target,0,n-1)
         
-        for(int i = 0; i < m; i++) { // O(m)
-            if(matrix[i][0] == target || matrix[i][n-1] == target) {
-                return true;
-            }
-            if(matrix[i][0] < target && matrix[i][n-1] > target) {
-                return binSearch(matrix[i],target,0,matrix[i].size()-1); // O(logn)
-            }
-        }
+                
         
-        return false;
-    }
-};
-// Time Complexity: O(mlogn)
-// Space Complexity: O(1)
