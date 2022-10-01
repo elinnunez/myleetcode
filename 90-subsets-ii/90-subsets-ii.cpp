4775e83de229ed1165a2d7689d7d9daf71051829
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-09-30 20:02:01 -0500

    Time: 0 ms (100.00%), Space: 16.6 MB (19.55%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    void backtrack(vector<int>& nums, vector<vector<int>>& output, set<vector<int>>& found, int i, vector<int> cur) {
+    void backtrack(vector<int>& nums, vector<vector<int>>& output, int i, vector<int> cur) {
         if(i >= nums.size()) {
-            if(!found.count(cur)) {
-                found.insert(cur);
-                output.push_back(cur);
-            }
+            output.push_back(cur);
             return;
         }
+        
         cur.push_back(nums[i]);
-        backtrack(nums,output,found,i+1,cur);
+        
+        backtrack(nums,output,i+1,cur);
+        
         cur.pop_back();
         
         int curNum = nums[i];
         
         while(i < nums.size() && nums[i] == curNum) i++;
         
-        backtrack(nums,output,found,i,cur);
+        backtrack(nums,output,i,cur);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> output;
-        set<vector<int>> found;
         
         sort(nums.begin(),nums.end());
         
-        backtrack(nums,output,found,0,{});
+        backtrack(nums,output,0,{});
         
         return output;
     }
