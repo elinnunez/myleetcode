c67d0eb51e0dadafe243d15378050d510985430f
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-10-02 16:40:24 -0500

    Time: 2 ms (59.62%), Space: 7.6 MB (77.69%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
+        int prev2 = 0;
+        int prev = 0;
         
-        vector<int>dp(nums.size(),-1);
-        
-        dp[0] = nums[0];
-        
-        for(int i = 1; i < nums.size(); i++) {
-            int pick = nums[i];
+        for(int i = 0; i < nums.size(); i++) {
             
-            if(i > 1) {
-                pick+= dp[i-2];
-            }
+            int pick = nums[i] + prev2;
+            int notpick = prev;
             
-            int notpick = dp[i-1];
+            prev2 = prev;
             
-            dp[i] = max(pick,notpick);
+            prev = max(pick,notpick);
+
         }
-        
-        return dp[n-1];
+
+        return prev;
     }
 };
 // Time Complexity: O(n)
-// Space Complexity: O(n)
+// Space Complexity: O(1)
