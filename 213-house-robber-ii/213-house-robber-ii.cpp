78585fc8fe2d4cdf45cfc8bdbbc6953d982161d8
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-10-02 18:36:06 -0500

    Time: 4 ms (40.56%), Space: 8.1 MB (19.89%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int getMax(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        
+        dp[0] = nums[0];
+        
+        for(int i = 1; i < nums.size(); i++) {
+            int take = nums[i];
+            if(i>1) take+= dp[i-2];
+            
+            int nottake = dp[i-1];
+            
+            dp[i] = max(take,nottake);
+        }
+        
+        return dp[nums.size()-1];
+    }
+    
+    int rob(vector<int>& nums) {
+        vector<int> part1;
+        vector<int> part2;
+        
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++) {
+            if(i != 0) part1.push_back(nums[i]);
+            if(i != n-1) part2.push_back(nums[i]);
+        }
+        
+        if(nums.size() == 1) {
+            return nums[0];
+        }
+        
+        return max(getMax(part1),getMax(part2));
+    }
+};
