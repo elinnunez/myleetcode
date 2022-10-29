29ccef9fba649fae63192e10354bda65560b105e
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-10-29 11:43:33 -0500

    Time: 51 ms (42.34%), Space: 13 MB (59.32%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
-//     int backtrack(string t1, string t2, int idx1, int idx2, vector<vector<int>>& dp) {
-//         if(idx1 < 0 || idx2 < 0) return 0;
-        
-//         if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
-        
-//         if(t1[idx1] == t2[idx2]) {
-//             return dp[idx1][idx2] = 1 + backtrack(t1,t2,idx1-1,idx2-1,dp);
-//         } else {
-//             return dp[idx1][idx2] = max(backtrack(t1,t2,idx1-1,idx2,dp), backtrack(t1,t2,idx1,idx2-1,dp));
-//         }
-        
-//     }
-    
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
         
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        
-        // for(int j = 0; j < m+1; j++) dp[0][j] = 0;
-        // for(int i = 0; i < n+1; i++) dp[i][0] = 0;
         
         for(int i = 1; i < n+1; i++) {
             for(int j = 1; j < m+1; j++) {
@@ -35,3 +19,5 @@ public:
         return dp[n][m];
     }
 };
+// Time Complexity: O(n*m)
+// Space Complexity: O(n*m)
