f0638ee63b43b5a95beddca0a2615ee2315f9871
Elinnoel Nuñez <48221285+elinnunez@users.noreply.github.com>
2022-10-28 21:03:04 -0500

    Time: 51 ms (42.34%), Space: 13.1 MB (24.78%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -19,11 +19,11 @@ public:
         
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         
-        for(int j = 0; j <= m; j++) dp[0][j] = 0;
-        for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        for(int j = 0; j < m+1; j++) dp[0][j] = 0;
+        for(int i = 0; i < n+1; i++) dp[i][0] = 0;
         
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
+        for(int i = 1; i < n+1; i++) {
+            for(int j = 1; j < m+1; j++) {
                 if(text1[i-1] == text2[j-1]) {
                     dp[i][j] = 1 + dp[i-1][j-1];
                 } else {
